f34b78e6683488400841568c1d9521c94ca023fe
lucasrn9 <lucasrn9.00@gmail.com>
2025-07-16 16:56:08 -0300

    WIP(ui): starts creating group event component

--- screens/groupEvents/GroupEvents.tsx
+++ screens/groupEvents/GroupEvents.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import {View} from 'react-native';
 import GroupHeader from '../../components/groupHeader/GroupHeader';
+import UserPhoto from '../../components/userPhoto/UserPhoto';
+import BubbleSpeech from '../../components/bubbleSpeech/BubbleSpeech';
 
 function GroupEvents() {
   return (
     <View>
       <GroupHeader groupName="Group Name" />
+      <UserPhoto />
+      <BubbleSpeech />
     </View>
   );
 }
